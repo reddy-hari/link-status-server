985c53c37b101fd4e659f6da0d7faa5dae77b392
hari <harireddy05@gmail.com>
2023-07-19 10:59:17 -0600

    [Refactors] Configuring cors

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 // server.js
 import express from "express";
 import fetch from "node-fetch";
+import { exec } from "child_process";
 import cors from "cors";
 
 const app = express();
 const PORT = 4000;
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 app.get("/proxy/:url(*)", async (req, res) => {
   try {
     const { url } = req.params;
     const response = await fetch(url);
     // const data = await response.text();
-    res.send(response.status);
+    res.sendStatus(response.status);
   } catch (error) {
-    res.status(500).send("Error fetching resource");
+    res.sendStatus(500).send("Error fetching resource");
   }
+
+  // const { url } = req.params;
+  // exec(`curl -I ${url}`, (error, stdout, stderr) => {
+  //   if (error) {
+  //     console.error(`Error Occurred - ${error}`);
+  //     res.status(500).send("Error fetching resource");
+  //   } else {
+  //     const statusMatch = stdout.match(/HTTP\/\d\.\d (\d+)/);
+  //     const statusCode = statusMatch ? parseInt(statusMatch[1]) : 500;
+  //     res.sendStatus(statusCode.toString());
+  //   }
+  // });
 });
 
 app.listen(PORT, () => {
